41e8640c373e518355f7d9e0f2ca29aa8eae2105
RusKom27 <48696676+RusKom27@users.noreply.github.com>
2023-02-25 23:09:40 +0200

    fix image sending

--- src/features/edit-user-form/components/EditUserForm.tsx
+++ src/features/edit-user-form/components/EditUserForm.tsx
@@ -1,10 +1,9 @@
 import React, {useState} from "react";
 import {Field, Formik} from "formik";
-import {useNavigate} from "react-router-dom";
 import * as Yup from "yup";
 
 import {Button, Image, Loader, TextInputField} from "../../../shared/ui";
-import {userApi} from "../../../shared/api";
+import {imageApi, userApi} from "../../../shared/api";
 import styles from "./EditUserForm.module.scss";
 import {useAppSelector} from "../../../shared/hooks";
 import {LoadImageButton} from "../../load-image-button";
@@ -12,10 +11,10 @@ import { FetchImage } from "../../../entities/image";
 
 
 export const EditUserForm = () => {
-    const navigate = useNavigate();
     const user_id = useAppSelector(state => state.auth.user_id);
     const {data: user} = userApi.useFetchUserByIdQuery(user_id);
     const [updateUser, {isSuccess}] = userApi.useUpdateUserMutation();
+    const [loadImage] = imageApi.useLoadImageMutation();
     const [avatarImage, setAvatarImage] = useState<File | null | undefined>(null);
     const [profileImage, setProfileImage] = useState<File | null | undefined>(null);
 
@@ -43,6 +42,8 @@ export const EditUserForm = () => {
                     .max(20, 'Must be 20 characters or less'),
             })}
             onSubmit={(values, { setSubmitting }) => {
+                if (avatarImage) loadImage(avatarImage);
+                if (profileImage) loadImage(profileImage);
                 updateUser({
                     images: {
                         avatar_image: {
